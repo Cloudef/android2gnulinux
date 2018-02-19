12f987e88031c5cbd9ff6534b9279b11219510ac
Jari Vetoniemi <mailroxas@gmail.com>
2018-02-19 12:46:46 +0200

    jvm-android: implement getSharedPreferences

--- src/libjvm-android.c
+++ src/libjvm-android.c
@@ -1,3 +1,5 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include <assert.h>
 #include "jvm/jni.h"
 
@@ -6,4 +8,14 @@ android_content_Context_getPackageName(JNIEnv *env, jobject object, va_list args
 {
    assert(env && object);
    return (*env)->NewStringUTF(env, "com.gnu.linux");
+}
+
+jobject
+android_content_Context_getSharedPreferences(JNIEnv *env, jobject object, va_list args)
+{
+   assert(env && object);
+   jstring str = va_arg(args, jstring);
+   printf("%s::%d\n", (*env)->GetStringUTFChars(env, str, NULL), va_arg(args, jint));
+   va_end(args);
+   return (*env)->AllocObject(env, (*env)->FindClass(env, "android/content/SharedPreferences"));
 }
