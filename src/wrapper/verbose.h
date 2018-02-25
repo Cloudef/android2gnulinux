b3c841834bc4edfb78ad32cad58fb1d69e5beb4d
Jari Vetoniemi <mailroxas@gmail.com>
2018-02-25 15:25:56 +0200

    verboase: Mutex logging
    We have threading in apps now.

--- src/wrapper/verbose.h
+++ src/wrapper/verbose.h
@@ -1,15 +1,21 @@
 #pragma once
 
 #ifdef VERBOSE_FUNCTIONS
+#  include <stdio.h>
 #  include <stdarg.h>
+#  include <pthread.h>
 static void
 verbose(const char *fmt, ...)
 {
+   static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+   pthread_mutex_lock(&mutex);
+   fprintf(stderr, "%lu: ", pthread_self());
    va_list ap;
    va_start(ap, fmt);
    vfprintf(stderr, fmt, ap);
    va_end(ap);
    fputc('\n', stderr);
+   pthread_mutex_unlock(&mutex);
 }
 #else
 #  define verbose(...)
