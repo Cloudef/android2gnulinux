da0993edb828d86b7e874e99b9865368b7b789a8
Jari Vetoniemi <mailroxas@gmail.com>
2018-02-27 13:40:45 +0200

    liblog: Implement functions

--- src/liblog.c
+++ src/liblog.c
@@ -1,19 +1,25 @@
 #include <stdarg.h>
-
-int
-__android_log_print(int prio, const char *tag,  const char *fmt, ...)
-{
-   return 0;
-}
+#include <stdio.h>
 
 int
 __android_log_vprint(int prio, const char *tag,  const char *fmt, va_list ap)
 {
-   return 0;
+   printf("(%d) %s :: ", prio, tag);
+   return vprintf(fmt, ap);
+}
+
+int
+__android_log_print(int prio, const char *tag,  const char *fmt, ...)
+{
+   va_list ap;
+   va_start(ap, fmt);
+   int ret = __android_log_vprint(prio, tag, fmt, ap);
+   va_end(ap);
+   return ret;
 }
 
 int
 __android_log_write(int prio, const char *tag, const char *text)
 {
-   return 0;
+   return __android_log_print(prio, tag, "%s", text);
 }
